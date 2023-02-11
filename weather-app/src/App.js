e9799cc3003415f3474ecf6d47d4ab2860d550b6
FarhanaRafi <118438210+FarhanaRafi@users.noreply.github.com>
2023-02-11 14:44:53 +0100

    done

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
 import Home from "./components/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
+import NotFoundPage from "./components/NotFoundPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ weather-app/src/components/NotFoundPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { NotFound } from "@mongez/react-components";
+
+const NotFoundPage = () => {
+  return (
+    <>
+      <NotFound />
+    </>
+  );
+};
+
+export default NotFoundPage;
